5a5d4b99322008616c8e3d9c7ae09b411223586f
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-11-26 15:00:43 +0300

    26/11 - var value by remember {MutableStateOf("0")}

--- app/src/main/java/com/tomaslab/composegooglepart3/MainActivity.kt
+++ app/src/main/java/com/tomaslab/composegooglepart3/MainActivity.kt
@@ -3,41 +3,69 @@ package com.tomaslab.composegooglepart3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.tomaslab.composegooglepart3.ui.theme.ComposeGooglePart3Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeGooglePart3Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+             Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TipTimeScreen()
                 }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TipTimeScreen(){
+    Column(modifier = Modifier
+        .padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+
+    ){
+
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+       EditNumberField()
+    }
+}
+
+@Composable
+fun EditNumberField(){
+    var amountInput by remember { mutableStateOf("") }
+    TextField(
+        value = amountInput,
+        onValueChange = {amountInput = it}
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeGooglePart3Theme {
-        Greeting("Android")
-    }
+        TipTimeScreen()
 }
